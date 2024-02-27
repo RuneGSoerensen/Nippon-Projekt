49a643546244ec289928e8ef85a8ebba0dd82fd3
RuneGSoerensen <eaa23rgso@students.eaaa.dk>
2024-02-27 21:41:49 +0100

    Javascript  added
    Javascript er tilføjet for:
    Tilføj og fjern knap til kurv tallet
    
    Javascript tilføjet for pop-up og luk for både måltidskasser og opskrifter.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+
+/* Javascript for at øger tallet ved indkøbs kurven for at simulere køb */
+
+
 const itemCountSpan = document.getElementById("itemCount");
 const koebBtn = document.querySelector(".koebBtn");
+const popUpWindow = document.querySelector(".background-blur-sektion");
+
 
 let itemCount = 0;
 
@@ -16,3 +22,16 @@ koebBtn.addEventListener('click', function(){
     }
     itemCountSpan.textContent = itemCount;
 });
+
+/* Javascript for at åbne og lukke pop up vinduer */
+
+function showPopUp() {
+    popUpWindow.style.display = 'block';
+}
+
+function hidePopUp() {
+    popUpWindow.style.display = 'none';
+}
+
+
+
